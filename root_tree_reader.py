4be71f1e8a09aa6379022ea92686d652822478f6
daniel ocampo <daniel.ocampoh@udea.edu.co>
2021-04-01 14:06:37 -0500

    modifying conditions to read jagged and non jagged branches

--- root_tree_reader.py
+++ root_tree_reader.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-# using uproot4 as uproot (4.0.6)
-# using awkward1 as awkward (ak 1.1.2 & ak1 1.0.0)
+# using uproot4 as uproot
+# using awkward1 as awkward 
 
 import numpy as np
 import pandas as pd
@@ -11,88 +10,64 @@ import awkward
 class RootTreeReader:
 
   """ 
-  Read and load data from a ROOT tree to a Pandas DataFrame
+  Read data from a ROOT TTree 
 
   Parameters:
   path : string
-      Path to the ROOT tree
+      Path to the ROOT file
 
   tree_name : string (default=Delphes)
-      Name of the ROOT tree
+      Name of the ROOT TTree
 
   Attributes:
-    tree: Root tree read
+    tree: Root TTree 
   """
 
   def __init__(self, path: str, tree_name: str = "Delphes"):
     self.tree = uproot.open(path)[tree_name]
 
 
-  def get_branches(self, branches, n_values=4) -> pd.DataFrame:
+  def get_branches(self, branches: list, max_elements=4):
     """
-    returns a pandas DataFrame with branches as columns
+    returns a DataFrame with branches as features
 
     branches : array-like
-      Branches to load from the ROOT tree
+      branches to load from the ROOT tree
 
-    n_values : int (default=4)
-      Number of values to load from jagged arrays
-    """
-    self.n_values = n_values
-    self.dataframe = pd.DataFrame(index=range(self.tree.num_entries))
+    max_elements : int (default=4)
+      maximum number of elements to load from jagged arrays
+    """   
+    self._max_elements = max_elements
+    self._df = pd.DataFrame(index=range(self.tree.num_entries))
 
     for branch in branches:
-      if type(self.tree[branch]) is uproot.models.TBranch.Model_TBranch_v13:
-        self._add_branch(branch)
-      else:
-        self._join_dataframe(branch) 
-    
-    return self.dataframe
+      self._join_branch(branch)
+
+    return self._set_columns_names(self._df)
 
 
-  def get_branch(self, branch: str, library="pd"):
-    """
-    returns a tree branch element
+  def _join_branch(self, branch):
+    """joins a branch to self._df"""
+    df = self.tree.arrays(branch, library="pd")
 
-    branch: string
-      Branch to load from the ROOT tree
-
-    library: string (default="pd")
-      The library that is used to represent arrays
-      pd : Pandas dataframe
-      ak : awkward array
-      np : numpy array
-    """
-    return self.tree.arrays(branch, library=library)
-
-
-  def _join_dataframe(self, branch):
-    """joins branch element to self.dataframe"""
-    df = self.get_branch(branch)
-
-    if df.index.get_level_values("subentry").any():
-
-      df = df.unstack().iloc[:,:self.n_values]
-      df.columns = [f"{branch}{i}" for i in range(self.n_values)]
-      self.dataframe = self._set_columns_names(self.dataframe.join(df))
-
+    if len(df) > self.tree.num_entries:
+      self._add_jagged_branch(df, branch)
     else:
-      df.reset_index(drop=True, level=1, inplace=True)
-      self.dataframe = self._set_columns_names(self.dataframe.join(df))
+      self._add_branch(df, branch)
 
 
-  def _add_branch(self, branch: str, name=None):
-    """adds a branch to self.dataframe"""
-    if not name:
-      name = branch.lower()
+  def _add_branch(self, df, branch: str):
+    """adds a non-jagged branch to self.dataframe"""
+    self._df[branch] = self.tree[branch].array(library="pd").values
 
-    self.dataframe[name] = self.tree[branch].array(library="pd")
 
+  def _add_jagged_branch(self, df, branch):
+    """adds a jagged branch to self.dataframe"""
+    df = df.unstack().iloc[:,:self._max_elements]
+    df.columns = [f"{branch}{i}" for i in range(self._max_elements)]
+    self._df = self._df.join(df)
 
   @staticmethod
   def _set_columns_names(df):
-    """
-    changes the column names of self.dataFrame
-    """
     df.columns = df.columns.str.lower().str.replace(".","_")
     return df
